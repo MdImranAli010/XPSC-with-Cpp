f37e120095b732b9b2e6b48f89cc4cc29c3eff03
MdImranAli010 <163654831+MdImranAli010@users.noreply.github.com>
2024-03-17 00:13:02 +0600

    Create Four Digits.cpp

---
+++ week-1/day-2/Four Digits.cpp
@@ -0,0 +1,15 @@
+/*
+    Author: Md. Imran Ali
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin>>n;
+    
+    cout<<setw(4)<<setfill('0')<<n;
+
+    return 0;
+}
