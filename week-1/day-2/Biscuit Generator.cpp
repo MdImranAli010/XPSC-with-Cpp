083e03193af24bf2fedfd0cf5fc844c2dafe60f2
MdImranAli010 <163654831+MdImranAli010@users.noreply.github.com>
2024-03-17 00:15:32 +0600

    Create Biscuit Generator.cpp

---
+++ week-1/day-2/Biscuit Generator.cpp
@@ -0,0 +1,16 @@
+/*
+    Author: Md. Imran Ali
+*/
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n,m, T;
+    cin>>n>>m>>T;
+    
+    cout<<(T/n)*m<<endl;
+
+    return 0;
+}
