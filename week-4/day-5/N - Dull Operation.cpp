21f33ae7ca6e7e27cbde44081386535d778978a6
MdImranAli010 <163654831+MdImranAli010@users.noreply.github.com>
2024-04-11 23:36:18 +0600

    N - Dull Operation.cpp

---
+++ week-4/day-5/N - Dull Operation.cpp
@@ -0,0 +1,18 @@
+// Author: Md. Imran Ali
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        long long n;
+        cin>>n;
+        
+        cout<<n<<" "<<n-1<<endl;
+    }
+    return 0;
+}
