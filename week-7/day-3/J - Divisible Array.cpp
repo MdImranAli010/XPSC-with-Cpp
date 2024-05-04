cb5965e5f06bbf7746c62a72343737c6ee7400f4
MdImranAli010 <163654831+MdImranAli010@users.noreply.github.com>
2024-05-05 01:45:19 +0600

    J - Divisible Array.cpp

---
+++ week-7/day-3/J - Divisible Array.cpp
@@ -0,0 +1,20 @@
+// Author: Md. Imran Ali
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    int t;
+    cin >> t;
+    while(t--){
+        int n;
+        cin>>n;
+        for (int i = 1; i <= n; i++) {
+            cout<<i*2<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;     
+ }
