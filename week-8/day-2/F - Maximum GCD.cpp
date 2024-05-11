1997bba0037402edf33ebbca3f8b9cf67396f639
MdImranAli010 <163654831+MdImranAli010@users.noreply.github.com>
2024-05-12 01:57:21 +0600

    F - Maximum GCD.cpp

---
+++ week-8/day-2/F - Maximum GCD.cpp
@@ -0,0 +1,23 @@
+// Author: Md. Imran Ali
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int t;
+    cin>>t;
+    while(t--){
+        long long n;
+        cin>>n;
+        
+        long long res=n/2;
+        cout<<res<<endl;
+    }
+    
+    return 0;
+}
